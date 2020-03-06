e0d1043d6f04bc7829498d0e69ef43adb900f459
rattle99 <24495512+rattle99@users.noreply.github.com>
2020-03-06 23:48:37 +0530

    Add GA

--- SimpleGA.m
+++ SimpleGA.m
@@ -5,7 +5,10 @@ clc
 G = tf(1, [1 1 1 0]);
 step(G)
 
-controller = pid(0.5, 0, 0.1);
+K = [0.5 0 0.1];
+controller = pid(K(1), K(2), K(3));
 OpenLoop = series(G,controller);
 ClosedLoop = feedback(OpenLoop,1);
 step(ClosedLoop)
+
+[x,fval] = ga(@(K)itae(G,K),3);
